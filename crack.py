46a6f025f4f5c08bb3e697e2b65c58f87b369d6b
anggamanggala21 <anggamanggala21@gmail.com>
2020-09-01 21:58:35 +0800

    Update for support python 3

--- crack.py
+++ crack.py
@@ -1,9 +1,14 @@
 import hashlib
+import platform
+
+if (int(platform.python_version_tuple()[0]) < 3):
+	try: input = raw_input
+	except NameError: raw_input = input
 
 flag = 0
 
-pass_hash = raw_input("Enter md5 hash : ")
-wordlist = raw_input("Enter wordlist file name : ")
+pass_hash = input("Enter md5 hash : ")
+wordlist = input("Enter wordlist file name : ")
 
 try:
 	pass_file = open(wordlist, "r")
